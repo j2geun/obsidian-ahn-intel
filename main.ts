7a9c70f94b73be3cab847e52bfddfa050519929a
jeonggeun <jeonggeun@jeonggeunui-MacBookPro.local>
2024-05-20 00:28:20 +0900

    read docs 기능 추가

--- main.ts
+++ main.ts
@@ -1,16 +1,21 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { htmlToMarkdown } from 'obsidian';
+import { htmlToMarkdown, requestUrl, sanitizeHTMLToDom } from 'obsidian';
+// import { JSDOM } from 'jsdom'
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
 	mySetting: string;
 	doscBaseUrl: string;
+	aceId: string;
+	acePw: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default',
-	doscBaseUrl: ''
+	doscBaseUrl: '',
+	aceId: '',
+	acePw: ''
 }
 
 export default class MyPlugin extends Plugin {
@@ -31,6 +36,12 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 		
+		this.addCommand({
+			id: 'test-get-html',
+			name: 'test get html',
+			callback: () => this.fetchDocsConent()
+		})
+
 		this.addCommand({
 			id: 'convert-html-to-markdown',
 			name: 'convert html to markdown',
@@ -112,6 +123,86 @@ export default class MyPlugin extends Plugin {
         // 사용자에게 작업 완료를 알립니다.
         new Notice('HTML has been converted to Markdown and saved as a new file.');
 	}
+	
+	async encodeCredentials(username: string, password: string): Promise<string> {
+		const credentials: string = `${username}:${password}`;
+		const encodeCredentials: string = Buffer.from(credentials).toString('base64');
+		return encodeCredentials;
+	}
+
+	async fetchDocsConent() {
+		const pageId = 826966264;
+		console.log(this.settings.aceId);
+		console.log(this.settings.acePw);
+		const username = this.settings.aceId;
+		const password = this.settings.aceId;
+
+		const encodedCredentials: string = await this.encodeCredentials(this.settings.aceId, this.settings.acePw);
+
+		const url = "https://docs.ahnlab.com/pages/viewpage.action?pageId=" + pageId
+		console.log(url)
+
+		console.log(encodedCredentials)
+
+		try {
+			const response = await requestUrl({
+				url: url,
+				headers: {
+					Authorization: `Basic ${encodedCredentials}`
+				}
+			});
+
+			const htmlText: string = response.text;
+			
+			const documentFragment: DocumentFragment = sanitizeHTMLToDom(htmlText)
+			const mainContentElement = documentFragment.querySelector('#main-content');
+			
+			if (mainContentElement) {
+				console.log(mainContentElement.outerHTML)
+				
+				const mainContentHTML: string = mainContentElement.outerHTML;
+				const markdown: string = htmlToMarkdown(mainContentHTML);
+
+				const filePath = `/converted-file.md`;
+        		await this.app.vault.create(filePath, markdown);
+			}
+
+			// const dom = new JSDOM(htmlText);
+			// const docsDocument = dom.window.document;
+
+			// const mainContentElement = document.getElementById('main-content');
+
+			// if (mainContentElement) {
+			// 	console.log('main-content : ', mainContentElement.textContent);
+			// } else {
+			// 	console.log('cant find main-content');
+			// }
+			
+		} catch (error) {
+			// 오류 처리
+			console.log('get content err: ', error.message);
+			console.log(error.body);
+		}
+		
+		// const options = {
+		// 	auth: `${this.settings.aceId}:${this.settings.acePw}`
+		// };
+
+		// const url = "https://docs.ahnlab.com/pages/viewpage.action?pageId=" + pageId
+
+		// try {
+		// 	const response: AxiosResponse = await axios.get(url, {
+		// 		auth: {
+		// 			username: this.settings.aceId,
+		// 			password: this.settings.acePw
+		// 		}
+		// 	});
+
+		// 	console.log('응답 데이터: ', response.data);
+		// } catch (error){
+		// 	console.error('get docs content err: ', error.message);
+		// }
+	}
 }
 
 class SampleModal extends Modal {
@@ -154,10 +245,34 @@ class TISettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				})
 			)
-
+		
+		containerEl.createEl('h2', { text: 'ACE Auth' });
 		new Setting(containerEl)
-			.setName("")
-			.setDesc("sync on load")
+			.setName("ACE ID")
+			.setDesc("ace id")
+			.addText(text => text
+				.setPlaceholder('Enter ACE ID')
+				.setValue(this.plugin.settings.aceId)
+				.onChange(async (value) => {
+					console.log('ACE ID: ' + value);
+					this.plugin.settings.aceId = value;
+					await this.plugin.saveSettings();
+				})
+			)
+		new Setting(containerEl)
+			.setName("ACE PW")
+			.setDesc("ace pw")
+			.addText(text => {
+				text.inputEl.type ='password';
+				text
+				.setPlaceholder('Enter ACE PW')
+				.setValue(this.plugin.settings.acePw)
+				.onChange(async (value) => {
+					console.log('ACE PW: ' + value);
+					this.plugin.settings.acePw = value;
+					await this.plugin.saveSettings();
+				});
+			});
 
 		new Setting(containerEl)
 			.setName('Setting #1')
